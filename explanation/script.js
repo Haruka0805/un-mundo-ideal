bb1ad9553f64730a798191df071d42ac92fc8d26
Haruka0805 <gc231116@senshu-u.jp>
2025-07-02 17:46:37 +0900

    UI変更

--- explanation/script.js
+++ explanation/script.js
@@ -15,3 +15,22 @@ function setupGlow(audioId, lineId) {
   setupGlow('audio2', 'line2');
   setupGlow('audio3', 'line3');
   setupGlow('audio4', 'line4');
+  function enableLineClick(audioId, lineBoxClass) {
+  const audio = document.getElementById(audioId);
+  const boxes = document.getElementsByClassName(lineBoxClass);
+
+  Array.from(boxes).forEach(box => {
+    box.addEventListener('click', () => {
+      if (!audio.paused) {
+        audio.currentTime = 0;
+      }
+      audio.play();
+    });
+  });
+}
+
+// 各line-boxにイベント付与（line-boxはすべて同じクラスなので1対1で対応）
+enableLineClick('audio1', 'line-box');
+enableLineClick('audio2', 'line-box');
+enableLineClick('audio3', 'line-box');
+enableLineClick('audio4', 'line-box');
